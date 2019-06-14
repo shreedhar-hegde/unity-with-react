34e82fae03e51df75fb394f84d1a15408fe5d607
Shreedhar Hegde <shreedharhegade96@gmail.com>
2019-06-14 18:34:10 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Unity, { UnityContent } from "react-unity-webgl";
+import { Button } from "react-bootstrap";
+import {
+  FaAngleDown,
+  FaAngleUp,
+  FaAngleLeft,
+  FaAngleRight
+} from "react-icons/fa";
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    // Next up create a new Unity Content object to
+    // initialise and define your WebGL build. The
+    // paths are relative from your index file.
+
+    this.unityContent = new UnityContent(
+      "/Build/Web.json",
+      "/Build/UnityLoader.js"
+    );
+  }
+
+  render() {
+    return (
+     
+        <Unity unityContent={this.unityContent} />
+  
+    );
+
+    // Finally render the Unity component and pass
+    // the Unity content through the props.
+  }
 }
-
-export default App;
---
+++ src/components/Buttons.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+import { Button } from 'react-bootstrap';
+
+class Buttons extends Component {
+
+    render() {
+        return(
+            <Button variant="primary">Primary</Button>
+        )
+    }
+}
+
+export default Buttons
